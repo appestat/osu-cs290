2e8a43d09ce2bbfb6d1e791338d3d67fed2d7df2
Kai <gayk@oregonstate.edu>
2018-08-10 16:09:17 -0700

    Added some models

---
+++ models/boards.js
@@ -0,0 +1,34 @@
+var db = require('mysql2');
+const config = require('../config/dbCredentials.json');
+var conpool = db.createPool(config);
+
+
+function retrieve(callback) {
+    conPool.query("select * from boards", (err, results) => {
+	    callback(err, results);
+    });
+};
+function retrieveThisBoard(data, callback) {
+    conPool.query("select name, games from boards where board=id = ?", [data.id], (err, results) => {
+	callback(err, results);
+    });
+}
+    
+function insert(data, callback) {
+    conPool.query("insert into boards(title, default_elo, default_rd, default_vol, tau) values (?, ?, ?, ?, ?)",[data.title, data.default_elo, data.default_rd, data.default_vol, data.tau], (err, results) => {
+ 	callback(err, results);
+    });
+}
+
+
+function update(data, callback) {
+    conPool.query("update boards set title=?, default_elo=?, default_rd=?, default_vol=?, tau=? where board_id=?", [data.title, data.default_elo, data.default_rd, data.default_vol, data.tau], (err, results) => {
+	callback(err, results);
+    });
+}
+
+function delete(data, callback) {
+    conPool.execute("DELETE from boards WHERE board_id=?", [data.id], (err, results) => {
+	callback(err, results);
+    });
+}
---
+++ models/users.js
@@ -0,0 +1,38 @@
+var db = require('mysql2');
+const config = require('../config/dbCredentials.json');
+var conpool = db.createPool(config);
+
+
+function retrieve(callback) {
+    conPool.query("select * from users", (err, results) => {
+	    callback(err, results);
+    });
+};
+
+function insert(data, callback) {
+    conPool.query("insert into users(profile, elo, RD, VOL, board) values (?, ?, ?, ?, ?)",[data.profile, data.elo, data.rd, data.vol, data.board_id], (err, results) => {
+	callback(err, results);
+    });
+}
+
+
+function update(data, callback) {
+    conPool.query("update users set elo=?, RD=?, VOL=? where user_id=?", [data.elo, data.rd, data.vol, data.user_id], (err, results) => {
+	callback(err, results);
+    });
+}
+
+function deleteUser(data, callback) {
+    conPool.execute("DELETE from users WHERE user_id=?", [data.id], (err, results) => {
+	callback(err, results);
+    });
+}
+		  
+		 
+module.exports = {
+    retrieveUser : retrieve,
+    insertUser : insert,
+    updateUser : update,
+    deleteUser : deleteUser
+}
+    
